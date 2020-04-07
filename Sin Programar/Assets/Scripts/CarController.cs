8aeb68e641a9018bc22fca31ef450eda492d2a89
EnzoR98 <enzo.rolon@outlook.com>
2020-04-07 18:29:06 -0300

    Car Movement
    Created and codified car movement script

---
+++ Sin Programar/Assets/Scripts/CarController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private InputPlayer input;
+    private Rigidbody rb;
+    public float speed;
+    public float rotationSpeed;
+    public float rotationAngle;
+    public float maxSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = GetComponent<InputPlayer>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(input.yAxis < -0.2f || input.yAxis > 0.2f)
+        {
+            rotationAngle += (input.xAxis * rotationSpeed);
+            if(rotationAngle > 360 || rotationAngle < -360)
+            {
+                rotationAngle = 0;
+            }
+        }
+        //Debug.Log(rigidbody.velocity.z);
+    }
+
+    private void FixedUpdate()
+    {
+        CarMovement();
+    }
+
+
+
+    void CarMovement()
+    {
+        //Accelerate, decelerate
+        rb.velocity = transform.forward * input.yAxis * speed * Time.deltaTime;
+
+        //Turn
+        transform.rotation = Quaternion.Euler(transform.rotation.x, rotationAngle, transform.rotation.z);
+    }
+}
---
+++ Sin Programar/Assets/Scripts/InputPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputPlayer : MonoBehaviour
+{
+    public float xAxis;
+    public float yAxis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xAxis = Input.GetAxis("Horizontal");
+        yAxis = Input.GetAxis("Vertical");
+    }
+}
