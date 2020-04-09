a860b7d4a1a43697c333955fb755e93b3a97579c
EnzoR98 <enzo.rolon@outlook.com>
2020-04-09 19:58:52 -0300

    ImmovableObstacle basic function
    Car controller needs to be improved to continue testing the immovable obstacle

--- Sin Programar/Assets/Scripts/Obstacles/ImmovableObstacle.cs
+++ Sin Programar/Assets/Scripts/Obstacles/ImmovableObstacle.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class ImmovableObstacle : MonoBehaviour
 {
+    public float pushForce;
+    public float jumpForce;
 
     private void OnCollisionEnter(Collision col)
     {
-
+        if (col.gameObject.CompareTag("Vehicle"))
+        {
+            CarController vehicle = col.gameObject.GetComponent<CarController>();
+            vehicle.rb.AddForce(new Vector3(0, 1 * jumpForce, -1 * pushForce), ForceMode.Impulse);
+        }
     }
 }
