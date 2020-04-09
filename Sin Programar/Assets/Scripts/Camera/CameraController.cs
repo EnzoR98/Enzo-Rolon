f594874b4b0f5c708903d5e6f18ed32c4a0199db
EnzoR98 <enzo.rolon@outlook.com>
2020-04-09 19:18:35 -0300

    CameraController done
    Camera controller script finished

--- Sin Programar/Assets/Scripts/Camera/CameraController.cs
+++ Sin Programar/Assets/Scripts/Camera/CameraController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    private Transform playerTransform;
+    public float altura;
+    public float distance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTransform = FindObjectOfType<CarController>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(0, altura, playerTransform.position.z - distance);
     }
 }
